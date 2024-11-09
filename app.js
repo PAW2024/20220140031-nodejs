db22ba9de0cbeb789fea787be5d8df451b2fddfc
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-09 08:24:42 +0700

    Mendesain Layout

--- app.js
+++ app.js
@@ -3,19 +3,35 @@ const app = express();
 const todoRoutes = require('./routes/tododb.js');
 require('dotenv').config();
 const port = process.env.PORT || 3030;
+const db = require('./database/db');
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts);
 app.use(express.json());
 
 app.use('/todos', todoRoutes)
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        layout: 'layouts/main-layout',
+    });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact',{
+        layout: 'layouts/main-layout',
+    });
 });
 
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
