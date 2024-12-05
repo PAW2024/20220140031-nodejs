5f0031f16745ded10d69072ad1aaa18c716e95c3
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-05 10:26:42 +0700

    STYLING_PAWA_031

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,23 +1,5 @@
-const express = require('express');
-const app = express();
-const todoRoutes = require('./routes/todo.js');
-const port = 3000;
-
-app.use(express.json());
-
-app.use('/todos', todoRoutes)
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/contact',isAuthenticated, (req, res) => {
+    res.render('contact',{
+        layout: 'layouts/main-layout',
+    });
 });
-
-app.get('/contact', (req, res) => {
-    res.render('contact');
-});
-
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
-
